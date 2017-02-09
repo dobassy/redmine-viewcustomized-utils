78c3e2450175e952f91e9de1d6c21f26be7a741e
Dobashi, Hiroki <hiroki.dobashi@gmail.com>
2017-02-09 13:05:28 +0900

    update a view customized scripts

---
+++ js/versions-set-version.js
@@ -0,0 +1,24 @@
+//
+// Path pattern: 	/issues/[0-9]+$
+//
+
+/*
+バージョンページで新規チケット作成時には引き継いでセットする
+Path pattern: /versions/[0-9]+$
+Type: JavaScript
+*/
+
+$(function () {
+  var href = window.location.href;
+  var reg = /versions\/([0-9]+)?/;
+  var version = href.match(reg)[1];
+
+  if (!version) {
+    return;
+  }
+
+  // 対象バージョンがあればリンクにパラメータ追加
+  var new_issue_link = $('a.new-issue-sub').attr("href");
+  new_issue_link += '?issue%5Bfixed_version_id%5D=' + version;
+  $('a.new-issue-sub').attr("href", new_issue_link);
+});
